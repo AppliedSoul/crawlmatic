3d855431731ec44ef9b934f6e4066d835cd47c63
noel <noelgaur@analyticservice.net>
2018-08-14 22:51:16 +0800

    Dynamic crawler test specs/cases.

---
+++ test/dynamic.spec.js
@@ -0,0 +1,37 @@
+const {
+  DynamicCrawler
+} = require('../index');
+
+
+describe('Dynamic Crawler Tests', function() {
+
+  const crawler = new DynamicCrawler({
+    evaluatePage: (() => ({
+      title: $('title').text(),
+    }))
+  });
+
+  it('setup should be fulfilled', function() {
+    return crawler.setup().should.eventually.be.fulfilled;
+  })
+
+  it('should able to dynamically crawl website (https://bing.com get Title)', function() {
+    return crawler.request({
+      url: 'https://bing.com'
+    }).then((res) => {
+      return res.result.title;
+    }).should.eventually.be.a('string');
+  })
+
+  it('should be able to reject if faulty requests are made', function() {
+    return crawler.request({
+      url: 'http://bingxxxx.comm',
+      timeout: 10 * 1000,
+      retryCount: 0
+    }).should.eventually.be.rejected;
+  })
+
+  it('should able to close the crawler instance', function() {
+    return crawler.destroy().should.eventually.be.fulfilled
+  })
+})
